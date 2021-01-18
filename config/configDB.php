01526a4ce46084efa57b352df080071561f33fce
Arkaitz Rodriguez Martinez <arkaitz.rodmar@educa.jcyl.es>
2021-01-18 13:11:51 +0100

    Cambios app

--- config/configDB.php
+++ config/configDB.php
@@ -1,15 +1,16 @@
 <?php
 //CONFIGURACIÓN ENTORNO DE DESARROLLO
-    
+    /*
     define('DNS', 'mysql:host=192.168.20.19:3306; dbname=DAW205DBProyectoTema5');//mysql:host=192.168.0.205:3306; dbname=DAW205DBDepartamentos
     define('USER', 'usuarioDAW205DBProyectoTema5');//usuarioDAW205DBDepartamentos
     define('PASSWORD', 'P@ssw0rd');//paso
+    */
     
    
     //CONFIGURACIÓN 1&1
-   /*
-    define('DNS', 'mysql:host=db5000278682.hosting-data.io;dbname=dbs272026');//Dirección IP y nombre de la base de datos
-    define('USER', 'dbu287777');//Nombre de usuario de la base de datos
-    define('PASSWORD', 'Covid1234$');//Contraseña del usuario de la base de datos
-    */
+   
+    define('DNS', 'mysql:host=db771560304.hosting-data.io:3306; dbname=db771560304');//mysql:host=192.168.0.205:3306; dbname=DAW205DBDepartamentos
+    define('USER', 'dbo771560304');//usuarioDAW205DBDepartamentos
+    define('PASSWORD', 'Covid1234$');//paso
+    
 ?>
--- controller/cInicio.php
+++ controller/cInicio.php
@@ -1,27 +1,23 @@
 <?php
-/**
- *   @author: Javier Nieto Lorenzo
- *   @since: 02/12/2020
- *   Programa
-*/
-if(!isset($_SESSION['usuarioDAW2LoginLogoffMulticapaPOO'])){ // si no se ha logueado le usuario
-    header('Location: index.php'); // redirige al login
+
+if(!isset($_SESSION['usuarioDAW2LoginLogoffMulticapaPOO'])){
+    header('Location: index.php');
     exit;
 }
 
-if (isset($_REQUEST['cerrarSesion'])) { // si se ha pulsado el boton de Cerrar Sesion
-    session_destroy(); // destruye todos los datos asociados a la sesion
-    header("Location: index.php"); // redirige al login
+if (isset($_REQUEST['cerrarSesion'])) {
+    session_destroy();
+    header("Location: index.php");
     exit;
 }
-$oUsuarioControlador = $_SESSION['usuarioDAW2LoginLogoffMulticapaPOO'];
+$oUsuarioActual = $_SESSION['usuarioDAW2LoginLogoffMulticapaPOO'];
 
-$numConexiones = $oUsuarioControlador->getNumConexiones(); // variable que tiene el numero de conexiones sacado de la base de datos
-$descUsuario = $_SESSION['usuarioDAW2LoginLogoffMulticapaPOO']->getDescUsuario(); // variable que tiene la descripcion del usuario sacado de la base de datos
-$ultimaConexion = $_SESSION['usuarioDAW2LoginLogoffMulticapaPOO']->getFechaHoraUltimaConexion();
-$imagenUsuario = $_SESSION['usuarioDAW2LoginLogoffMulticapaPOO']->getImagenPerfil();
+$numConexiones = $oUsuarioActual->getNumConexiones();
+$descUsuario = $oUsuarioActual->getDescUsuario();
+$ultimaConexion = $oUsuarioActual->getFechaHoraUltimaConexion();
+$imagenUsuario = $oUsuarioActual->getImagenPerfil();
 
-$vista = $vistas['inicio'];
+$vistaEnCurso = $vistas['inicio'];
 require_once $vistas['layout'];
 
 ?>

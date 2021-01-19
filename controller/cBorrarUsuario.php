14d8454b2c2a6f770d0065d85f9a2d99982cb058
Arkaitz Rodriguez Martinez <arkaitz.rodmar@educa.jcyl.es>
2021-01-19 13:45:26 +0100

    Cambios app

--- controller/cBorrarUsuario.php
+++ controller/cBorrarUsuario.php
@@ -1,3 +1,13 @@
 <?php
-
+    if(!isset($_SESSION['usuarioDAW2LoginLogoffMulticapaPOO'])){ // si no se ha logueado le usuario
+        header('Location: index.php'); // redirige al login
+        exit;
+    }
+    if(isset($_REQUEST['borrarUsuario'])){
+        $oUsuario = UsuarioPDO::borrarUsuario($_REQUEST['CodUsuario']);
+        header("Location: index.php");
+        exit;
+    }
+    $vistaEnCurso = $vistas['borrarUsuario']; // guardamos en la variable vistaEnCurso la vista que queremos implementar
+    require_once $vistas['layout'];
 ?>

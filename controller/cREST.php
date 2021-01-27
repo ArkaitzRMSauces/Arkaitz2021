a103e9e7b85e029170bf6d2ddb6e83df8248e7ef
Arkaitz Rodriguez Martinez <arkaitz.rodmar@educa.jcyl.es>
2021-01-27 11:48:48 +0100

    Cambios app añandido rest e implementado 21/01/26

--- controller/cREST.php
+++ controller/cREST.php
@@ -1,12 +1,22 @@
 <?php
-    if(!isset($_SESSION['usuarioDAW2LoginLogoffMulticapaPOO'])){ // si no se ha logueado le usuario
-        header('Location: index.php'); // redirige al login
-        exit;
+if(isset($_REQUEST['volver'])){
+    $_SESSION['paginaEnCurso'] = $controladores['inicio'];
+    header("Location: index.php");        
+    exit;
+}
+try{
+    if(isset($_REQUEST['enviar'])) { //si se ha enviado una fecha
+        //llamamos al servicio y le pasamos la fecha introducida por el usuario
+        $aServicioAPOD = REST::sevicioAPOD($_REQUEST['fecha']);
+    }else {
+        //llamamos al servicio y le pasamos la fecha de hoy
+        $aServicioAPOD = REST::sevicioAPOD(date('Y-m-d'));
     }
-    if(isset($_REQUEST['volver'])){
-        header("Location: index.php");
-        exit;
-    }
-    $vistaEnCurso = $vistas['rest']; // guardamos en la variable vistaEnCurso la vista que queremos implementar
-    require_once $vistas['layout'];
-?>
+    $tituloEnCurso = $aServicioAPOD['title'];
+    $imagenEnCurso = $aServicioAPOD['url'];
+    $descripcionEnCurso = $aServicioAPOD['explanation'];
+} catch (Exception $error){
+    echo 'Error: ',  $error->getMessage(), "\n";
+}
+$vistaEnCurso = $vistas['rest']; //variable que contiene la vista que va a ejecutarse
+require_once $vistas['layout']; //llamamos al layout
--- view/vREST.php
+++ view/vREST.php
@@ -1,6 +1,37 @@
-<?php 
-    echo "REST";
-?>
-<form name="inicio" method="post">
-    <button type="submit" name="volver">Volver</button>
-</form>
+<!--
+    Autor: Susana Fabián Antón
+    Fecha creación: 26/01/2021
+    Última modificación: 26/01/2021
+-->
+<header>
+    <h1>REST</h1>
+</header>
+<article>
+    <section>
+        <div class=".formMain">
+            <p>Uso de API REST</p>
+            <div>
+                <p>APOD: Atronomy Picture of the Day</p>
+                <p>Puedes seleccionar una fecha para ver su imagen</p>
+                <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+                    <p>
+                        <input type="date" name="fecha" value="<?php echo date('Y-m-d') ?>">
+                    </p>
+                    <div>
+                        <input type="submit" value="Enviar" name="enviar">
+                    </div>
+                </form>
+            </div>
+            <div>
+                <p><?php echo $tituloEnCurso?></p>
+                <img src="<?php echo $imagenEnCurso?>" width="100">
+                <p><?php echo $descripcionEnCurso?></p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+            <button class="button" type="submit" name="volver">Volver</button>
+        </form>
+    </section>
+</article>

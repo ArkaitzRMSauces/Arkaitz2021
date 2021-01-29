4c1d1beb5c8f8de8f544b5e41ebecd6d4344df92
Arkaitz Rodriguez Martinez <arkaitz.rodmar@educa.jcyl.es>
2021-01-29 09:01:34 +0100

    Cambios app añandido rest e implementado 21/01/29

--- view/layout.php
+++ view/layout.php
@@ -16,6 +16,7 @@
         <footer>
             <a href="http://daw205.ieslossauces.es/" target="_blank"> <img src="webroot/media/oneandone.png" alt="oneandone icon" width="40"></a>
             <address><a href="../../index.html">&copy; 2020-2021 Arkaitz Rodriguez Martinez</a></address>
+            <address><a href="https://www.apple.com/" target="_blank">WEB IMITADA</a></address>
             <a href="https://github.com/ArkaitzRMSauces/Arkaitz2021" target="_blank"><img class="github" width="40" src="webroot/media/github.png" ></a>
         </footer>
     </body>

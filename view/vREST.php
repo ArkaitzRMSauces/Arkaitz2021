fa3d324c55eb12c64ad3fd60c7da8d4b39720df9
Arkaitz Rodriguez Martinez <arkaitz.rodmar@educa.jcyl.es>
2021-02-17 10:49:54 +0100

    Cambios app 21/02/17 Añadido menu de navegacion en el rest

--- view/vREST.php
+++ view/vREST.php
@@ -5,6 +5,13 @@
 -->
 <header>
     <h1>REST</h1>
+    <div>
+        <form class="navInicio" name="logout" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <button type="submit" name="miCuenta"><img src="webroot/media/miCuenta.jpg" width="25px" height="25px"></button>
+            <button type="submit" name="borrarUsuario"><img src="webroot/media/borrarUsuario.png" width="25px" height="25px"></button>
+            <button type="submit" name="cerrarSesion"><img src="webroot/media/cerrarSesion.png" width="25px" height="25px"></button>
+        </form>
+    </div>
 </header>
 <article class="articleREST">
     <section class="sectionREST1">
